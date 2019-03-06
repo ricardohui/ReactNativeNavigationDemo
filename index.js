568599ae3ff588967c900c2921b0c7737897e874
Ricardo <eehaha@gmail.com>
2019-03-07 07:01:27 +0800

    Add react native navigation

--- index.js
+++ index.js
@@ -3,8 +3,16 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { Navigation } from "react-native-navigation";
+import App from "./App";
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => App);
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: "navigation.playground.WelcomeScreen"
+      }
+    }
+  });
+});
