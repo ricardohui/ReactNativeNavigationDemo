c9cdf5c97c6f190a83bfb46534b6d5c23f16147f
Ricardo <eehaha@gmail.com>
2019-03-08 08:16:34 +0800

    two tabs

--- index.js
+++ index.js
@@ -4,14 +4,52 @@
  */
 
 import { Navigation } from "react-native-navigation";
-import App from "./App";
+import App from "./src/App";
+import ScreenA from "./src/screens/ScreenA";
+import Icon from "react-native-vector-icons/MaterialIcons";
+Navigation.registerComponent(`navigation.playground.TextScreen`, () => App);
+Navigation.registerComponent(`navigation.playground.ScreenA`, () => ScreenA);
+Navigation.events().registerAppLaunchedListener(async () => {
+  const sources = await Promise.all([
+    Icon.getImageSource("call-split", 30),
+    Icon.getImageSource("add", 30),
+    Icon.getImageSource("menu", 30)
+  ]);
 
-Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => App);
-Navigation.events().registerAppLaunchedListener(() => {
   Navigation.setRoot({
     root: {
-      component: {
-        name: "navigation.playground.WelcomeScreen"
+      bottomTabs: {
+        children: [
+          {
+            component: {
+              name: "navigation.playground.TextScreen",
+              passProps: {
+                text: "This is tab 1",
+                myFunction: () => "Hello from a function!"
+              },
+              options: {
+                bottomTab: {
+                  text: "Tab 1",
+                  icon: sources[0]
+                }
+              }
+            }
+          },
+          {
+            component: {
+              name: "navigation.playground.ScreenA",
+              passProps: {
+                text: "This is tab 2"
+              },
+              options: {
+                bottomTab: {
+                  text: "Tab 2",
+                  icon: sources[1]
+                }
+              }
+            }
+          }
+        ]
       }
     }
   });
